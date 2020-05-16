e8ae96a7d8a74ea1f42c150c02828b5cd44f9f53
Giedrius <266934@via.dk>
2020-05-16 22:02:44 +0200

    Add student table with dummy data

--- src/app/sections/section-students/section-students.component.ts
+++ src/app/sections/section-students/section-students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/shared/student';
 
 @Component({
   selector: 'app-section-students',
@@ -8,6 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class SectionStudentsComponent implements OnInit {
 
   constructor() { }
+
+  students: Student[] = [
+    {id: 1, initials: "KO", age: 21, studyStart: new Date(2019,9,1), country: "Spain"},
+    {id: 2, initials: "JS", age: 25, studyStart: new Date(2019,9,1), country: "Italy"},
+    {id: 3, initials: "ER", age: 23, studyStart: new Date(2019,9,1), country: "Denmark"},
+    {id: 4, initials: "SJ", age: 22, studyStart: new Date(2019,9,1), country: "Spain"},
+    {id: 5, initials: "GS", age: 21, studyStart: new Date(2019,9,1), country: "Denmark"},
+    {id: 6, initials: "AK", age: 24, studyStart: new Date(2019,9,1), country: "Estonia"},
+    {id: 7, initials: "JB", age: 25, studyStart: new Date(2019,9,1), country: "Spain"},
+    {id: 8, initials: "TO", age: 23, studyStart: new Date(2019,9,1), country: "Italy"},
+  ];
 
   ngOnInit(): void {
   }
---
+++ src/app/shared/student.ts
@@ -0,0 +1,7 @@
+export interface Student {
+    id: number;
+    initials: string;
+    age: number;
+    studyStart: Date;
+    country: string;
+}
